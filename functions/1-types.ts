528001bda3e10254e545159ff4cbbfb0f4722a02
Nathalia Prado <nathalia_898@hotmail.com>
2023-08-08 22:02:10 +1200

    add getFullName implementation

--- functions/1-types.ts
+++ functions/1-types.ts
@@ -6,7 +6,9 @@ export function getGreeting(name: string): string {
 
 // getFullName should return a string containing the contents
 // of `firstName` and the contents of `lastName`
-export function getFullName(firstName: string, lastName: string): void {}
+export function getFullName(firstName: string, lastName: string): string {
+    return firstName + " " + lastName
+}
 
 // addNumbers should return the sum of the two parameters
 export function addNumbers(): void {}
